237038f36df4808a352f9f1592d14f0f89330563
unknown <AlwaysDeveloper>
2020-07-31 18:04:50 +0530

    update

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ const userRoutes = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+
+    next();
+});
 //express.static(path.join(__dirname,'./static'))
 app.use(['/audio'],(req,res,next)=>{res.set('content-type', 'audio/mp3');res.set('accept-ranges', 'bytes');res.sendFile(path.join(__dirname,`./statics${req.originalUrl}`));});
 app.use(['/image'],(req,res,next)=>{res.set('content-type', 'image/png');res.set('accept-ranges', 'bytes');res.sendFile(path.join(__dirname,`./statics${req.originalUrl}`));});
